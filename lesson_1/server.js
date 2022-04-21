6fbdba43a4e430a01ff6d1e034e277f7a9bce342
Tsukasa Muto <73716890+tsukasamuto55@users.noreply.github.com>
2022-04-21 01:23:20 -0600

    changes port

--- lesson_1/server.js
+++ lesson_1/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000; 
+const port = process.env.PORT || 3000;
 const path = require('path');
 
 app.set('view engine', 'ejs');
