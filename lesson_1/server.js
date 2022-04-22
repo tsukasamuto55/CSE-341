f8794da7c4494e7be973dab2a6a56ad08513ed70
Tsukasa Muto <73716890+tsukasamuto55@users.noreply.github.com>
2022-04-22 15:33:58 -0600

    Creates views and public directories

--- lesson_1/server.js
+++ lesson_1/server.js
@@ -2,13 +2,18 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const path = require('path');
+const familyData = require('./data.json');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use('/', require('./routes'))
+app.use(express.static("public"));
 
-app.get('/home', (req, res) => {
-  res.render('home');
+
+
+app.get('/family', (req, res) => {
+  const data = familyData
+  res.render('family', {...data});
 })
 
 app.listen(port, (err) => {
