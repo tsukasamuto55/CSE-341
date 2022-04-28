2070ea788e44950c9c9f8432768287b58f323d3b
Tsukasa Muto <73716890+tsukasamuto55@users.noreply.github.com>
2022-04-27 19:54:41 -0600

    add path

--- week_2/server.js
+++ week_2/server.js
@@ -2,15 +2,17 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const path = require('path');
-const familyData = require('./data.json');
+
 require('dotenv').config();
 require('./connection');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.set('public', path.join(__dirname, 'public'));
 app.use('/', require('./routes'))
 app.use(express.static("public"));
 
+const familyData = require('./data.json');
 
 
 app.get('/family', (req, res) => {
