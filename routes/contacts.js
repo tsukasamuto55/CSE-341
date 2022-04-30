64c72c35642dacc72963bfe8d7bbb06073073ca8
Tsukasa Muto <73716890+tsukasamuto55@users.noreply.github.com>
2022-04-30 15:42:01 -0600

    Refactored routes

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,11 +1,24 @@
 const routes = require('express').Router();
 const connect = require('../db/connect');
+const ObjectId = require('mongodb').ObjectId;
 
-routes.get('/contacts', (req, res) => {
-   connect.getCollection().find().toArray((err, result) => {
-       if (err) throw err;
-       res.json(result);
-       console.log('Contacts query Succesful')
+routes.get('/', (req, res) => {
+    const results = connect.getCollection().find();
+    results.toArray().then((documents) => {
+        res.status(200).json(documents);
+        console.log('Returned All Contacts');
+   });
+});
+
+routes.get('/:id', (req, res) => {
+    const contactId = new ObjectId(req.params.id);
+    
+    const results = connect.getCollection().find({_id: contactId});
+
+
+    results.toArray().then((documents) => {
+        res.status(200).json(documents[0]);
+        console.log(`Returned First Contact: ${req.params.id}`);
    });
 });
 
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,12 @@
 const routes = require('express').Router();
 const familyData = require('../data.json');
 
-routes.get('/', (req, res) => {
-    res.send('Emily Muto is my wife. Please add "/family" at the end of URL to see all of my family members.')
-})
+routes.use('/home', (require('./home')))
 
-routes.get('/contacts', (require('./contacts')));
+routes.use('/contacts', (require('./contacts')));
 
-routes.get('/family', (req, res) => {
-  const data = familyData
+routes.use('/family', (req, res) => {
+  const data = familyData;
   res.render('family', {...data});
 })
 
